295ffea5a3fd3b660c3070d19c7af56002c66516
XuBin8866 <1205817366@qq.com>
2020-08-19 00:58:12 +0800

    优化了AspectListExecutor的被代理方法与切入点表达式的匹配过滤逻辑，解决了长久以来的随机出现的切面丢失问题

--- src/main/java/com/xxbb/framework/simplespring/aop/AspectListExecutor.java
+++ src/main/java/com/xxbb/framework/simplespring/aop/AspectListExecutor.java
@@ -8,6 +8,7 @@ import net.sf.cglib.proxy.MethodInterceptor;
 import net.sf.cglib.proxy.MethodProxy;
 
 import java.lang.reflect.Method;
+import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 
@@ -36,28 +37,28 @@ public class AspectListExecutor implements MethodInterceptor {
     public Object intercept(Object o, Method method, Object[] args, MethodProxy methodProxy) throws Throwable {
         Object returnValue=null;
         //对切面方法进行精确筛选
-        collectAccurateMatchedAspectList(method);
+        List<AspectInfo> accurateAspectInfoList = collectAccurateMatchedAspectList(method);
         //没有切面对方法进行增强的情况
-        if(ValidationUtil.isEmpty(sortedAspectInfoList)){
+        if(ValidationUtil.isEmpty(accurateAspectInfoList)){
             LogUtil.getLogger().warn("There is no Aspect for this bean");
             returnValue=methodProxy.invokeSuper(o,args);
             return returnValue;
         }
         //1.按照order的顺序升序执行完所有的Aspect的before方法
-        invokeBeforeAdvices(method,args);
+        invokeBeforeAdvices(accurateAspectInfoList,method,args);
 
         try {
             //2.执行被代理类的方法
             returnValue=methodProxy.invokeSuper(o, args);
             //3.如果代理方法返回正常，则安装order的顺序降序执行完所有的Aspect的afterRunning方法
             //目前后置通知不对返回结果returnValue进行处理
-            invokeAfterReturningAdvices(method,args,returnValue);
+            invokeAfterReturningAdvices(accurateAspectInfoList,method,args,returnValue);
         }catch (Exception e){
             //4.如果代理方法返回异常，则安装order的顺序降序执行完所有的Aspect的afterThrowing方法
-            invokeAfterThrowingAdvices(method,args,e);
+            invokeAfterThrowingAdvices(accurateAspectInfoList,method,args,e);
         }finally{
             //最终通知
-            invokeAfterAdvices(method,args);
+            invokeAfterAdvices(accurateAspectInfoList,method,args);
         }
 
         return returnValue;
@@ -66,14 +67,16 @@ public class AspectListExecutor implements MethodInterceptor {
     /**
      * 根据当前被代理的目标方法精确筛选切面方法
      * @param method 被代理的目标方法
+     * @return List<AspectInfo> 每次经过方法过滤筛选出的集合
      */
-    private void collectAccurateMatchedAspectList(Method method) {
+    private List<AspectInfo> collectAccurateMatchedAspectList(Method method) {
         if(ValidationUtil.isEmpty(sortedAspectInfoList)){
-            return;
+            return null;
         }
+        List<AspectInfo> accurateAspectInfoList = new ArrayList<>(sortedAspectInfoList);
         //将不需要的切面方法去除
-        sortedAspectInfoList.removeIf(aspectInfo -> !aspectInfo.getPointcutLocator().accurateMatches(method));
-
+        accurateAspectInfoList.removeIf(aspectInfo -> !aspectInfo.getPointcutLocator().accurateMatches(method));
+        return accurateAspectInfoList;
     }
 
 
@@ -83,9 +86,9 @@ public class AspectListExecutor implements MethodInterceptor {
      * @param args 方法参数
      * @throws Throwable 异常
      */
-    private void invokeAfterAdvices(Method method, Object[] args) throws Throwable {
-        for(int i=sortedAspectInfoList.size()-1;i>=0;i--){
-            sortedAspectInfoList.get(i).getAspectObject().after(targetClass,method,args);
+    private void invokeAfterAdvices(List<AspectInfo> accurateAspectInfoList,Method method, Object[] args) throws Throwable {
+        for(int i=accurateAspectInfoList.size()-1;i>=0;i--){
+            accurateAspectInfoList.get(i).getAspectObject().after(targetClass,method,args);
         }
     }
 
@@ -95,9 +98,9 @@ public class AspectListExecutor implements MethodInterceptor {
      * @param args 方法参数
      * @param e 异常传入值
      */
-    private void invokeAfterThrowingAdvices(Method method, Object[] args, Exception e) throws Throwable {
-        for(int i=sortedAspectInfoList.size()-1;i>=0;i--){
-            sortedAspectInfoList.get(i).getAspectObject().afterThrowing(targetClass,method,args,e);
+    private void invokeAfterThrowingAdvices(List<AspectInfo> accurateAspectInfoList,Method method, Object[] args, Exception e) throws Throwable {
+        for(int i=accurateAspectInfoList.size()-1;i>=0;i--){
+            accurateAspectInfoList.get(i).getAspectObject().afterThrowing(targetClass,method,args,e);
         }
     }
 
@@ -108,12 +111,12 @@ public class AspectListExecutor implements MethodInterceptor {
      * @param returnValue 传入返回值
      * @return 返回值
      */
-    private Object invokeAfterReturningAdvices(Method method, Object[] args, Object returnValue) throws Throwable {
+    private Object invokeAfterReturningAdvices(List<AspectInfo> accurateAspectInfoList,Method method, Object[] args, Object returnValue) throws Throwable {
         // 虽然result每次都被刷新，但再没有特殊处理的情况下他一直都是被代理方法的返回值
         // 也就是每次的返回值在默认情况下就是被反射执行的方法的返回值
         Object result=null;
-        for(int i=sortedAspectInfoList.size()-1;i>=0;i--){
-            result=sortedAspectInfoList.get(i).getAspectObject().afterReturning(targetClass,method,args,returnValue);
+        for(int i=accurateAspectInfoList.size()-1;i>=0;i--){
+            result=accurateAspectInfoList.get(i).getAspectObject().afterReturning(targetClass,method,args,returnValue);
 
         }
         return result;
@@ -124,8 +127,8 @@ public class AspectListExecutor implements MethodInterceptor {
      * @param method 方法
      * @param args 方法参数
      */
-    private void invokeBeforeAdvices(Method method, Object[] args) throws Throwable {
-        for(AspectInfo aspectInfo:sortedAspectInfoList){
+    private void invokeBeforeAdvices(List<AspectInfo> accurateAspectInfoList,Method method, Object[] args) throws Throwable {
+        for(AspectInfo aspectInfo:accurateAspectInfoList){
             aspectInfo.getAspectObject().before(targetClass,method,args);
         }
     }
